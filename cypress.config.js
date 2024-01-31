676c13e599901df2b4a0509135b3d21de0497d51
Praveen Jayaprasad <praveenjnair2000@gmail.com>
2024-01-31 16:32:13 +0530

    ss

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,21 @@
 const { defineConfig } = require("cypress");
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 module.exports = defineConfig({
+  screenshotsFolder: 'cypress/screenshots',
+  allureResultsPath: 'allure-results',
+  screenshotOnRunFailure: true,
+  videoUploadOnPasses: false,
+  animationDistanceThreshold: 5,
+  downloadsFolder: 'cypress/downloads',
+  responseTimeout: 30000,
+  testingType: 'e2e',
+  trashAssetsBeforeRuns: false,
+  videoCompression: 15,
+  videosFolder: 'cypress/videos',
+  watchForFileChanges: true,
+  modifyObstructiveCode: false,
+  video: true,
+  record: false,
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
--- cypress/e2e/spec2.cy.js
+++ cypress/e2e/spec2.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passesss', () => {
     cy.visit('www.google.com')
-   // cy.get('.csssss').should('be.visible')
+    cy.get('.csssss').should('be.visible')
   })
 })
