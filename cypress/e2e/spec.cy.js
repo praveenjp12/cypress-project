16ca9adfe59f931d67ca2658460e3a4f34c42e21
Praveen Jayaprasad <praveenjnair2000@gmail.com>
2024-02-01 13:33:19 +0530

    env11

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
-    const data= {'Browser':`${Cypress.browser.name}`,
-    'Test Tags':`${Cypress.env('TEST_TAGS')}`,
-    'Tet Stage':`${Cypress.env('TEST_STAGE')}`}
-    cy.allure().writeEnvironmentInfo(data).parameter('initial', JSON.strongify(data, null, 2))
+    const data= {Browser:Cypress.browser.name,
+      Test_Tags:Cypress.env('TEST_TAGS'),
+      Test_Stage:Cypress.env('TEST_STAGE')}
+      cy.allure().writeEnvironmentInfo(data).parameter('initial', JSON.strongify(data, null, 2))
     cy.visit('https://example.cypress.io')
   })
 })
--- cypress/e2e/spec2.cy.js
+++ cypress/e2e/spec2.cy.js
@@ -1,8 +1,8 @@
 describe('template spec', () => {
   it('passesss', () => {
-    const data= {'Browser':`${Cypress.browser.name}`,
-    'Test Tags':`${Cypress.env('TEST_TAGS')}`,
-    'Tet Stage':`${Cypress.env('TEST_STAGE')}`}
+    const data= {Browser:Cypress.browser.name,
+    Test_Tags:Cypress.env('TEST_TAGS'),
+    Test_Stage:Cypress.env('TEST_STAGE')}
     cy.allure().writeEnvironmentInfo(data).parameter('initial', JSON.strongify(data, null, 2))
     cy.visit('www.google.com')
     cy.get('.csssss').should('be.visible')
