1558387c95d064cc6ad7f0c09eee34ae0df032c7
Praveen Jayaprasad <praveenjnair2000@gmail.com>
2024-02-01 13:28:41 +0530

    env10

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,8 @@
 describe('template spec', () => {
   it('passes', () => {
-    const data= {'Browser':Cypress.browser.name,
-    'Test Tags':Cypress.env('TEST_TAGS'),
-    'Tet Stage':Cypress.env('TEST_STAGE')}
+    const data= {'Browser':`${Cypress.browser.name}`,
+    'Test Tags':`${Cypress.env('TEST_TAGS')}`,
+    'Tet Stage':`${Cypress.env('TEST_STAGE')}`}
     cy.allure().writeEnvironmentInfo(data).parameter('initial', JSON.strongify(data, null, 2))
     cy.visit('https://example.cypress.io')
   })
