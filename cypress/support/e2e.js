13a0372650f7ef301f2d4a57cbd3b7e64e982d6b
Praveen Jayaprasad <praveenjnair2000@gmail.com>
2024-02-01 11:52:09 +0530

    env

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -17,5 +17,16 @@
 import './commands'
 require('@shelex/cypress-allure-plugin');
 
+cy.allure.writeEnvironmentInfo().parameter(
+    'Browser', Cypress.browser.name
+)
+
+cy.allure.writeEnvironmentInfo().parameter(
+    'Test-Tags', Cypress.env('TEST_TAGS')
+)
+
+cy.allure.writeEnvironmentInfo().parameter(
+    'Environment', Cypress.env('TEST_STAGE')
+)
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
