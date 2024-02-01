7d9b7e474b2b4cd5b0a5e097769e9b5438bc4197
Praveen Jayaprasad <praveenjnair2000@gmail.com>
2024-02-01 12:56:17 +0530

    env5

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require("cypress");
 const allureWriter = require("@shelex/cypress-allure-plugin/writer");
-import { configureAllureAdapterPlugins } from '@mmisty/cypress-allure-adapter/plugins';
 module.exports = defineConfig({
   screenshotsFolder: 'cypress/screenshots',
   allureResultsPath: 'allure-results',
@@ -20,20 +19,6 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
-      const reporter = configureAllureAdapterPlugins(on, config);
-         
-      // after that you can use allure to make operations on cypress start,
-      // or on before run start
-      on('before:run', details => {
-         reporter?.writeEnvironmentInfo({
-            info: {
-               Browser: Cypress.browser.name ,
-               TEST_TAGS: Cypress.env('TEST_TAGS'),
-               TEST_STAGE: Cypress.env('TEST_STAGE'),
-            },
-         });
-      });
-      // implement node event listeners here
     },
   },
   experimentalWebKitSupport: true,
