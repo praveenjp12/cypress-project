9f96ee1c1f1a1d3b7d713c2a97c9c55667e4a73a
Praveen Jayaprasad <praveenjnair2000@gmail.com>
2024-02-01 13:59:55 +0530

    env12

--- cypress/e2e/spec2.cy.js
+++ cypress/e2e/spec2.cy.js
@@ -3,7 +3,7 @@ describe('template spec', () => {
     const data= {Browser:Cypress.browser.name,
     Test_Tags:Cypress.env('TEST_TAGS'),
     Test_Stage:Cypress.env('TEST_STAGE')}
-    cy.allure().writeEnvironmentInfo(data).parameter('initial', JSON.strongify(data, null, 2))
+    cy.allure().writeEnvironmentInfo(data).parameter('initial', JSON.stringify(data, null, 2))
     cy.visit('www.google.com')
     cy.get('.csssss').should('be.visible')
   })
