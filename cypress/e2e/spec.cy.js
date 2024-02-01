3d9eae97e27a75fa4a45c66a88dd53d384e24f39
Praveen Jayaprasad <praveenjnair2000@gmail.com>
2024-02-01 12:19:39 +0530

    env2

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,16 @@
 describe('template spec', () => {
   it('passes', () => {
+    cy.allure().writeEnvironmentInfo().parameter(
+      'Browser', Cypress.browser.name
+  )
+  
+  cy.allure().writeEnvironmentInfo().parameter(
+      'Test-Tags', Cypress.env('TEST_TAGS')
+  )
+  
+  cy.allure().writeEnvironmentInfo().parameter(
+      'Environment', Cypress.env('TEST_STAGE')
+  )
     cy.visit('https://example.cypress.io')
   })
 })
